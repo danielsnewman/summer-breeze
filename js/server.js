84ab585a3b5a943f13b0f2e416aa1f30a367d0d1
danielsnewman <danielsnewman1@gmail.com>
2017-12-18 17:44:21 -0600

    Added bootstrap3 theme 'Yeti' to Full Calendar
    Added bootstrap3 theme 'Yeti' to Full Calendar

---
+++ js/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8080, () =>{
+  console.log('app running on port 8080');
+});
