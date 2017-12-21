e8d01d85fb55eb836338c5304d9247afcce66380
danielsnewman <danielsnewman1@gmail.com>
2017-12-21 08:43:30 -0600

    Added Mongoose to package.json & added public folder for file serving

--- js/server.js
+++ js/server.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
