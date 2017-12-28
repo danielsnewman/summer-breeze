e743dc21e83a4a952b8ca4b35337767d119778f2
danielsnewman <danielsnewman1@gmail.com>
2017-12-27 20:24:44 -0600

    Added some links to nav bar and wrote routes for them
    Added Rates & Availability and Amenities links to nav bar and wrote
    routes for them in index.js file.
    
    Still need to add and link the rest of the nav bar.

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,17 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
+router.get('/availibility', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/availibility.html'));
+});
+
+router.get('/amenities', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/amenities.html'));
+});
+
 router.post('/', (req, res) => {
 
 });
+
 
 module.exports = router;
