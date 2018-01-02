763264ccc2f2e6217547e29a03c1b93fb2e59f27
danielsnewman <danielsnewman1@gmail.com>
2018-01-02 15:29:26 -0600

    Styled Angular Price collector and added stepper functionality
    ***Still need to edit default value for select element.***

--- public/js/angularEstimator.js
+++ public/js/angularEstimator.js
@@ -1,15 +1,23 @@
-var app = angular.module('myApp', []);
+const app = angular.module('priceEstimator', ['revolunet.stepper']);
 
-app.controller("Ctrl",($scope) => {
-  $scope.Math=Math;
-  $scope.products = [{
-    value: 2100,
-    label: 'Fall/Winter'
-    }, {
-    value: 2900,
-    label: 'Spring'
-    },{
-    value: 3995,
-    label: 'Summer'
-  }];
+app.controller("weekStepper",($scope) => {
+  $scope.rating = 0;
+  $scope.minRating = 0;
+  $scope.maxRating = 8;
+});
+
+app.controller("dayStepper",($scope) => {
+  $scope.rating = 0;
+  $scope.minRating = 0;
+  $scope.maxRating = 6;
+});
+
+app.controller("seasonSelector",($scope) => {
+  $scope.Math = Math;
+  $scope.seasons = [
+    { label:'-', value: '$' + 0},
+    { label:'Fall/Winter', value: '$' + Math.floor((2100 + 245) * 1.115) },
+    { label: 'Spring', value: '$' + Math.floor((2900 + 245) * 1.115) },
+    { label: 'Summer', value: '$' + Math.floor((3995 + 245) * 1.115) }
+  ];
 });
