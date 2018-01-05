0c4dd0c2106787dfe54aa002b23b8f44080a8c0a
danielsnewman <danielsnewman1@gmail.com>
2018-01-05 16:15:42 -0600

    Converted NaN string to number in calculation
    Was able to convert HTML string expression to number in the controller
    using parseInt() method.
    
    **Full Calculator still not working**

--- public/js/angularEstimator.js
+++ public/js/angularEstimator.js
@@ -3,10 +3,10 @@ const app = angular.module('priceEstimator', ['revolunet.stepper']);
 app.factory('seasonFactory', () => {
   let factory = {};
   let seasons = [
-    { label:'-', value: '$' + 0},
-    { label:'Fall/Winter', value: '$' + Math.floor((2100 + 245) * 1.115) },
-    { label: 'Spring', value: '$' + Math.floor((2900 + 245) * 1.115) },
-    { label: 'Summer', value: '$' + Math.floor((3995 + 245) * 1.115) }
+    { label:'-', value: parseInt(0, 10)},
+    { label:'Fall/Winter', value: parseInt(Math.floor((2100 + 245) * 1.115), 10) },
+    { label: 'Spring', value: parseInt(Math.floor((2900 + 245) * 1.115), 10) },
+    { label: 'Summer', value: parseInt(Math.floor((3995 + 245) * 1.115), 10) }
   ];
   factory.getSeasons = () => {
     return seasons;
@@ -14,20 +14,27 @@ app.factory('seasonFactory', () => {
   return factory;
 });
 
-app.controller("weekStepper",($scope) => {
-  $scope.rating = 0;
-  $scope.minRating = 0;
-  $scope.maxRating = 8;
+app.controller("weekStepper",($scope, seasonFactory) => {
+  $scope.seasonFactory = seasonFactory.getSeasons();
+  $scope.weeks = 0;
+  $scope.minWeeks = 0;
+  $scope.maxWeeks = 8;
+  $scope.weekValues = { fallWinter: parseInt(2100, 10), spring: parseInt(2900, 10), summer: parseInt(3995, 10)}
 });
 
 app.controller("dayStepper",($scope) => {
-  $scope.rating = 0;
-  $scope.minRating = 0;
-  $scope.maxRating = 6;
+  $scope.day = 0;
+  $scope.minDay = 0;
+  $scope.maxDay = 6;
+  $scope.dayValues = { fallWinter: parseInt(300, 10), spring: parseInt(300, 10), summer: parseInt(300, 10)}
+
 });
 
-app.controller("seasonSelector",($scope, seasonFactory) => {
+app.controller("seasonSelector",($scope) => {
   $scope.Math = Math;
-  $scope.seasons = seasonFactory.getSeasons();
+  $scope.seasons = $scope.seasonFactory;
   $scope.seasonsList = $scope.seasons[0];
+  $scope.parentWeeks = $scope.weekValues;
+  $scope.parentdays = $scope.dayValues;
+
 });
