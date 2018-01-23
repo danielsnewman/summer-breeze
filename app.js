14456e308850dc776a8ee97b4c897dbc1cd10771
danielsnewman <danielsnewman1@gmail.com>
2018-01-23 10:26:29 -0600

    Last commit before attempting Pug integration

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const flash = require('connect-flash');
 const errorHandlers = require('./handlers/errorHandlers');
-
-
+const expressValidator = require('express-validator');
+const expressSession = require('express-session');
 
 const app = express();
+
+app.use(expressValidator());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(expressSession({secret: 'summer_breeze', saveUninitialized: false, resave: false}));
+
+app.use(flash());
 
 app.use('/', routes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,23 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-
-
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../public/index.html'));
-});
-
-router.get('/availibility', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../public/availibility.html'));
 });
 
 router.get('/amenities', (req, res) => {
   res.sendFile(path.join(__dirname + '/../public/amenities.html'));
 });
 
-router.post('/', (req, res) => {
+router.get('/availibility', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/availibility.html'));
+});
 
+router.post('/availibility', (req, res) => {
+  req.check('name', 'Name must be longer than 2 letters').isLength({min:2});
+  req.check('email','Must be a valid email').isEmail();
+  let errors = req.validationErrors();
+  console.log(errors);
 });
 
 
